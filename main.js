1b3557117a8cd4b7f6926fa0b90a890485bb55a2
MatthewChan123 <80291912+MatthewChan123@users.noreply.github.com>
2021-11-17 19:01:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+    canvas = create.Canvas(300, 300);
+    canvas.position(110,250);
+}
+function draw() {
+
+}
+function save() {
+    save('your_image.png');
+}
